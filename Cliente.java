f80d1a0901d926845e1b82730e390c9cfa12d7f3
charlysalmeida <charlys.almeida@gmail.com>
2016-07-11 13:56:30 -0300

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula2;
+
+/**
+ *
+ * @author usuario
+ */
+public class Cliente {
+    public String nome;
+    private String endereco;
+    private String cpf;
+    public int idade;
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    
+}
+
---
+++ Teste.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula2;
+
+/**
+ *
+ * @author usuario
+ */
+public class Teste {
+    public static void main(String args[]){
+        //criando um cliente
+        Cliente c1 = new Cliente();
+        c1.nome="Tesla";
+        c1.setCpf("12345678");
+        c1.setEndereco("Rua do programador");
+        c1.idade=30;
+        
+        //criando uma conta
+        Conta conta1 = new Conta();
+        conta1.setNumeroConta(1);
+        conta1.setLimite(1000);
+        conta1.setSaldo(500);
+        conta1.setTitular(c1);
+        
+        //Apresentando os dados anteriormente informados
+        System.out.println("Cliente: "+conta1.getTitular());
+        System.out.println("Conta: "+conta1.getNumeroConta());
+        System.out.println("Saldo: "+conta1.getSaldo());
+        System.out.println("Limite: "+conta1.getLimite());
+    }
+            
+}
